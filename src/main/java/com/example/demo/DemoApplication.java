b55cfc78743ab5349dc92002ad957fd98fc16b52
vaca <wsmslgh@gmail.com>
2022-03-14 01:42:37 -0700

    rtuhjgtedyfgjnrtfhgdfgfdgsefrfdvvxchbxdcfcgb

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +12,14 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
 
 }
--- src/main/java/com/example/demo/UserInfoController.java
+++ src/main/java/com/example/demo/UserInfoController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,18 @@ public class UserInfoController {
     @Autowired
     private UserInfoRepository userInfoRepository;
 
+
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
+    @GetMapping("/send/{input}")
+    public void sendFoo(@PathVariable String input) {
+        this.template.send("topic_input", input);
+    }
+    @KafkaListener(id = "webGroup", topics = "topic_input")
+    public void listen(String input) {
+        System.out.println("input value: {}" +input);
+    }
     /**
      * 查
      * @return
