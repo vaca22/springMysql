0c53efbf6c76f2f0c9808c5584d19634a1e5a151
vaca <wsmslgh@gmail.com>
2022-03-14 01:43:15 -0700

    rtuhjgtedyfgjnrtfhgdfgfdgsefrfdvvxchbxdcfcgbdsfgh

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,13 +10,13 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
+ /*   @Bean
     public NewTopic topic() {
         return TopicBuilder.name("topic1")
                 .partitions(10)
                 .replicas(1)
                 .build();
     }
-
+*/
 
 }
