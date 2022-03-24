d7a3abf1d019e631de1ba0eb7c18b3c61bd0b44c
vaca <wsmslgh@gmail.com>
2022-03-24 02:02:22 -0700

    sadfsdsadfxcbcchmnvsaedfedrgf5tggfvghtf5454ghghh

--- src/main/java/com/example/demo/UserInfoController.java
+++ src/main/java/com/example/demo/UserInfoController.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
 import com.example.demo.requestback.RegisterReturn;
-import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.TimeZone;
 
 @RestController
 @RequestMapping("/cloud-napi/v1")
@@ -24,13 +22,7 @@ public class UserInfoController {
     }
 
 
-    private KafkaTemplate<Object, Object> template;
 
-
-    @Autowired
-    public void setTemplate(KafkaTemplate<Object, Object> template) {
-        this.template = template;
-    }
 
 
     @PostMapping(value = "/register")
@@ -48,28 +40,7 @@ public class UserInfoController {
 
     }
 
-    @PostMapping(value = "/napi/v1/msg")
-    public String sendMsg() {
-        String msg="hello";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        sdf.setTimeZone( TimeZone.getTimeZone("GMT+08:00"));
-        String time=sdf.format(System.currentTimeMillis());
 
-        JsonObject msg1= new JsonObject();
-        msg1.addProperty("msg",msg);
-        msg1.addProperty("readTime",time);
-
-
-        return msg1.toString();
-
-    }
-
-    @PostMapping(value = "/napi/v1/bp/data/upload")
-    public String uploadPc100(@RequestBody String user) {
-        System.out.println(user.toString());
-        return "good";
-
-    }
 
     @PostMapping(value = "/login")
     public RegisterReturn login(@RequestBody UserLogin user) {
@@ -86,85 +57,8 @@ public class UserInfoController {
         if (!n1.equals(user.getPassword())) {
             return new RegisterReturn(4, "the phone has been registered", null);
         }
-
-
         return new RegisterReturn(1, "register success", null);
-
-
     }
 
-
-    @GetMapping("/send/{input}")
-    public void sendFoo(@PathVariable String input) {
-        this.template.send("fuck", input);
-    }
-    @KafkaListener(id = "webGroup", topics = "topic_input")
-    public void listen(String input) {
-        System.out.println("input value: {}" +input);
-    }
-    /**
-     * 查
-     * @return
-     */
-    @GetMapping(value = "/list")
-    public List<UserLogin> getUserList(){
-        return userInfoRepository.findAll();
-    }
-
-    /**
-     * 增
-     * @param username
-     * @param password
-     * @return
-     */
-    @PostMapping(value = "/addUser")
-    public UserLogin addUser(@RequestParam("phone") String phone,
-                             @RequestParam("password") String password){
-        UserLogin user = new UserLogin();
-        user.setPhone(phone);
-        user.setPassword(password);
-        return userInfoRepository.save(user);
-    }
-
-
-    @PostMapping(value = "/fuck")
-    public UserLogin addUser(@RequestBody UserLogin nx){
-        List<UserLogin>  d= userInfoRepository.findByPassword("sdf");
-        return d.get(0);
-    }
-
-    @GetMapping(value = "/fuck")
-    public List<UserLogin> addUser(){
-        List<UserLogin>  d= userInfoRepository.findByPassword("sdf");
-        return userInfoRepository.retrieveByName2("sdf");
-    }
-
-    /**
-     * 改
-     * @param id
-     * @param username
-     * @param password
-     * @return
-     */
-    @PutMapping(value = "updUser/{phone}")
-    public UserLogin updUser(
-                            @PathVariable("phone") String phone,
-                            @RequestParam("password") String password){
-        UserLogin user = new UserLogin();
-        user.setPhone(phone);
-        user.setPassword(password);
-        return userInfoRepository.save(user);
-    }
-
-    /**
-     * 删
-     * @param id
-     */
-    @DeleteMapping(value = "delUser/{phone}")
-    public void delUser(@PathVariable("phone") String phone){
-        UserLogin user = new UserLogin();
-        user.setPhone(phone);
-        userInfoRepository.delete(user);
-    }
 
 }
